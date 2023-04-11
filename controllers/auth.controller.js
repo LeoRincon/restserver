e335f273f18053b694ffa729365410e5df0f30fe
Leonardo Rincon <jleobotache@gmail.com>
2023-04-11 09:48:06 -0500

    feat: validate email, password, and state in the login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,13 +1,48 @@
 const { response, request } = require('express');
+const bcryptjs = require('bcryptjs');
 
-const login = (req = request, res = response) => {
+const User = require('../models/user');
+
+const login = async (req = request, res = response) => {
  const { email, password } = req.body;
 
- res.json({
-  msg: 'login',
-  email,
-  password,
- });
+ try {
+  // email exist?
+  const user = await User.findOne({ email });
+  if (!user) {
+   return res.status(400).json({
+    msg: 'email or password are incorrect - email',
+   });
+  }
+
+  // user active?
+  if (!user.state) {
+   return res.status(400).json({
+    msg: 'email or password are incorrect - state: false',
+   });
+  }
+
+  // password correct?
+  const validPassword = bcryptjs.compareSync(password, user.password);
+  if (!validPassword) {
+   return res.status(400).json({
+    msg: 'email or password are incorrect - password',
+   });
+  }
+
+  //generate a JWT
+
+  res.json({
+   msg: 'login',
+   email,
+   password,
+  });
+ } catch (error) {
+  console.log(error);
+  return res.status(500).json({
+   msg: 'error in the server, contact the administrator',
+  });
+ }
 };
 
 module.exports = {
